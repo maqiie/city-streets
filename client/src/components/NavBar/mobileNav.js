12b888c29fb1b4369fbb664c22f5caea6fbfc28a
maqiie <candiemp2580@gmail.com>
2023-06-07 03:26:14 +0300

    navbar

---
+++ client/src/components/NavBar/mobileNav.js
@@ -0,0 +1,100 @@
+
+import React, { useRef, useState } from "react";
+import { Link, BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab, faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { TweenMax, Power3 } from "gsap";
+import styles from "./MobileNav.css";
+
+library.add(fab, faBars);
+
+const MobileNav = ({
+  width,
+  logoUrl,
+  background,
+  navLinks,
+  socialIcon,
+  sticky,
+}) => {
+  let nav = useRef(null);
+  const [click, setClick] = useState(false);
+  const socialLinks = socialIcon.map((icon, index) => (
+    <li key={index}>
+      <a target="_blank" href={icon.url}>
+        <FontAwesomeIcon icon={icon.icon} />
+      </a>
+    </li>
+  ));
+  const handelExpand = () => {
+    if (click === false) {
+      TweenMax.to(nav, 0.8, { height: 300, ease: Power3.easeOut });
+      setClick(true);
+    } else {
+      TweenMax.to(nav, 0.8, { height: 0, ease: Power3.easeOut });
+      setClick(false);
+    }
+  };
+
+  return (
+    <div style={{ minHeight: "100vh", width: "100%" }}>
+      <Router>
+        <div
+          className={styles.MoNavContainer}
+          style={{
+            background: background,
+            position: sticky ? "sticky" : "unset",
+          }}
+        >
+          <div className={styles.mobileNav} style={{ background: background }}>
+            <div className={styles.navBars}>
+              <FontAwesomeIcon icon={faBars} onClick={handelExpand} />
+            </div>
+            <div className={styles.MoNavLogo}>
+              <img src={logoUrl} alt="logo" />
+            </div>
+
+            <div className={styles.MoNavSocial}>
+              {width > 700 ? <ul>{socialLinks}</ul> : null}
+            </div>
+          </div>
+          <div
+            className={styles.MoNavLinks}
+            ref={(el) => {
+              nav = el;
+            }}
+          >
+            <ul>
+              {navLinks.map((link, i) => (
+                <li key={i}>
+                  <Link to={link.to}>{link.name}</Link>
+                </li>
+              ))}
+            </ul>
+            {width < 700 ? (
+              <div className={styles.mobileNavII}>
+                <div className={styles.MoNavSocialII}>
+                  <ul>{socialLinks}</ul>
+                </div>
+              </div>
+            ) : null}
+          </div>
+        </div>
+
+        <Switch>
+          {navLinks.map((link, i) => (
+            <Route
+              key={i}
+              exact
+              path={`/${link.to}`}
+              component={link.component}
+            />
+          ))}
+        </Switch>
+      </Router>
+    </div>
+  );
+};
+
+export default MobileNav;
---
+++ client/src/components/NavBar/navbar.js
@@ -0,0 +1,158 @@
+import React, { useState, useEffect } from "react";
+import { Controller, Scene } from "react-scrollmagic";
+import { Tween, Timeline } from "react-gsap";
+import {
+  Link,
+  NavLink,
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import styles from "./styles.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import MobileNav from "./MobileNav";
+library.add(fab);
+
+export const ReactNavbar = ({ color, logo, menu, social, sticky }) => {
+  const [navLinks, setNavLinks] = useState([
+    { name: "HOME", to: "/" },
+    { name: "ARTICLES", to: "/articles" },
+    { name: "ABOUT ME", to: "/about" },
+    { name: "CONTACT", to: "/contact" },
+  ]);
+  const [socialIcon, setSocialIcon] = useState([
+    {
+      name: "Linkedin",
+      url: "https://www.linkedin.com/in/nazeh-taha/",
+      icon: ["fab", "linkedin-in"],
+    },
+    {
+      name: "Facebook",
+      url: "https://www.facebook.com/nazeh200/",
+      icon: ["fab", "facebook-f"],
+    },
+    {
+      name: "Instagram",
+      url: "https://www.instagram.com/nazeh_taha/",
+      icon: ["fab", "instagram"],
+    },
+    {
+      name: "Twitter",
+      url: "http://nazehtaha.herokuapp.com/",
+      icon: ["fab", "twitter"],
+    },
+  ]);
+  const [background, setBackground] = useState("rgb(25, 25, 25)");
+  const [logoUrl, setLogoUrl] = useState("https://svgshare.com/i/KHh.svg");
+  const [width, setWidth] = useState(window.innerWidth);
+  const updateWidthAndHeight = () => {
+    setWidth(window.innerWidth);
+  };
+  useEffect(() => {
+    menu ? setNavLinks(menu) : [];
+    color ? setBackground(color) : null;
+    logo ? setLogoUrl(logo) : null;
+    social ? setSocialIcon(social) : [];
+  }, []);
+  useEffect(() => {
+    window.addEventListener("resize", updateWidthAndHeight);
+    return () => window.removeEventListener("resize", updateWidthAndHeight);
+  });
+
+  if (width < 1150) {
+    return (
+      <MobileNav
+        width={width}
+        logoUrl={logoUrl}
+        background={background}
+        navLinks={navLinks}
+        socialIcon={socialIcon}
+        sticky={sticky}
+      />
+    );
+  }
+
+  const Error = () => <h1>Please pass the component in the menu prop</h1>;
+
+  return (
+    <div style={{ minHeight: "100vh", width: "100%" }}>
+      <Router>
+        <Controller>
+          <Scene triggerHook="onLeave" duration={300} pin>
+            {(progress) => (
+              <Timeline totalProgress={progress} paused>
+                <Tween
+                  from={{ height: "150px" }}
+                  to={{ height: "80px", background: background }}
+                >
+                  <div className={styles.header}>
+                    <div className={styles.navLogo}>
+                      <Link to="/">
+                        <div className="logo-container">
+                          <Timeline totalProgress={progress} paused>
+                            <Tween
+                              from={{ height: "150px" }}
+                              to={{ height: "70px" }}
+                            >
+                              <img
+                                className={styles.LogoImg}
+                                src={logoUrl}
+                                alt="logo"
+                              />
+                            </Tween>
+                          </Timeline>
+                        </div>
+                      </Link>
+                    </div>
+
+                    <div className={styles.navLinks}>
+                      <ul>
+                        {navLinks.map((link, i) => (
+                          <li key={i}>
+                            <NavLink
+                              exact
+                              to={link.to}
+                              activeClassName={styles.home}
+                            >
+                              {link.name}
+                            </NavLink>
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
+                    <div className={styles.navSocial}>
+                      <ul>
+                        {socialIcon.map((icon, i) => (
+                          <li key={i}>
+                            <a target="_blank" href={icon.url}>
+                              <FontAwesomeIcon icon={icon.icon} />
+                            </a>
+                            <span className={styles.tooltiptext}>
+                              {icon.name}
+                            </span>
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
+                  </div>
+                </Tween>
+              </Timeline>
+            )}
+          </Scene>
+        </Controller>
+        <Switch>
+          {navLinks.map((link, i) => (
+            <Route
+              key={i}
+              exact
+              path={link.to}
+              component={link.component ? link.component : Error}
+            />
+          ))}
+        </Switch>
+      </Router>
+    </div>
+  );
+};
